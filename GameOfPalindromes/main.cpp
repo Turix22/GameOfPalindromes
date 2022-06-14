b16b74a99cfd0759eca99063a2275a971b206ebf
Alexa <alexaturica@yahoo.com>
2022-06-14 15:32:10 +0300

    Initial Commit

---
+++ GameOfPalindromes/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  GameOfPalindromes
+//
+//  Created by Alexandra Turica on 14.06.22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
